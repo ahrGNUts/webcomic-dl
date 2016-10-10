5dc2ce5d85d5ec39089fa1433a93209772e5641e
Devon Sawatzky <d@dn3s.me>
2016-10-10 02:30:03 -0500

    refactored all per-comic-overrideable stuff into one superclass

--- lib/comic.py
+++ lib/comic.py
@@ -1,16 +1,38 @@
+class getComic(url:string):
+    """Return a new Comic object for the given url"""
+    pass
+
 class Comic:
+    #the CSS selector for the "next" link
+    nextSelector=""
+    #the CSS selector for the comic title
+    titleSelector=""
+    #the CSS selector for the comic img
+    imgSelector=""
+    #the CSS selector for supplemental text
+    textSelector=""
+    #the title of the site
+    siteTitle=""
+    #the default directory name to download into
+    defaultDirname=""
+    #the regex for matching the URL to the Comic
+    urlRegex=".*"
+    @classmethod
+    def match(cls, url:string):
+        """Returns whether this Comic class will work for the given URL"""
+        return re.match(cls.urlRegex, url)
+    
     def __init__(self, url:string):
         """Creates a Comic object, downloads and parses the comic page"""
         self.url=url
         self.dom=None
-    
+
     def getText(self, selector:string):
         """Return the text of the first element matching the given selector"""
 
     def getAttr(self, selector:string, attr:string):
         """Return the value of the given attribute for the first element matching the given selector"""
 
-    nextSelector=""
     def getNext(self):
         """Return the URL of the next page if there is one, or False otherwise"""
         return self.getHref(nextSelector)
@@ -19,12 +41,10 @@ class Comic:
         """Return the page number"""
         return 0
 
-    titleSelector=""
     def getTitle(self):
         """Return the title of this comic"""
         return self.getHref(self.titleSelector)
 
-    imgSelector=""
     def getImg(self):
         """Return the image URL for this page"""
         return self.getAttr(self.imgSelector, "src")
@@ -33,7 +53,6 @@ class Comic:
         """Return the alt text for this comic"""
         return self.getAttr(self.imgSelector, "alt")
 
-    textSelector=""
     def getAccompanyingText(self):
         """Return any accompanying text for this comic"""
         return  getText(self.textSelector)
