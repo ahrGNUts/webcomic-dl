2186e70243f44b8dd53f4338baa95c310a8fae8d
Devon Sawatzky <d@dn3s.me>
2016-10-30 20:31:20 -0500

    upadated CLI

--- webcomic_dl/cli.py
+++ webcomic_dl/cli.py
@@ -4,15 +4,20 @@ def getArgs():
 	p=argparse.ArgumentParser(
 			description="Download a webcomic archive"
 			)
-	p.add_argument("url",
-			help="the URL of the webcomic to comic download"
+	p.add_argument("comic",
+			help="the name or URL of the webcomic to comic download"
 			)
-	p.add_argument("-n", "--max",
+	p.add_argument("-c", "--count",
 			dest="max",
 			metavar="n",
 			default=0,
 			type=int,
 			help="Maximum number of comics to download. 0 means unlimited"
+			)
+	p.add_argument("-f", "--from-url",
+			help="By default, webcomic-dl will download from the first webcomic in the series, regardless of the URL. This flag overrides it. For some comics that don't have numbers associated with their comics, this may cause naming collisions if you later decide to go back and download older comics. Thus, only use this if you are either certain you will never want earlier comics, or are willing to re-download the newer ones."
+			dest="from",
+			action="store_true",
 			)
 	p.add_argument("-d", "--dir",
 			dest="dir",
@@ -28,7 +33,7 @@ def getArgs():
         p.add_argument("-n", "--no-resume",
                         dest="resume",
                         action="store_false",
-                        help="By default, webcomic-dl will resume downloads from where it last left off. This flag overrides it. Still does not download already-downloaded comics unless combined with -o."
+                        help="By default, webcomic-dl will resume downloads from where it last left off. This flag overrides it. Does not download already-downloaded comics unless -o is also used, but still needs to download the webpage of each comic"
 	p.add_argument("-f", "--file",
 			dest="file",
 			help="Specify where to save metadata. By default it is saved in <output_dir>/info.json",
